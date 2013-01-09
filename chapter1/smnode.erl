95c46f6cff0c1e4efd160457448b6147fc4c293b
psyomn <lethaljellybean@gmail.com>
2013-01-08 23:55:07 -0500

    Defined a small, simple state machine

--- chapter1/smnode.erl
+++ chapter1/smnode.erl
@@ -2,12 +2,28 @@
 -author("lethaljellybean@gmail.com"). 
 -export([smachine/2]).
 
+% TODO 
 % @doc This is a node that implements behaviour of a state machine
 % in a perhaps naive method. 
 %
 % A state machine requires vertices, edges, and conditions to travel
 % on one edge to another. 
+%
+%                 t0
+%                 /\
+%  q0 --- t1 ---> q1 -+ t1
+%  /\                   |
+%   `---- t0 ---- q2 -- /
+%                 V t1
 
+sample_sm() ->
+  smachine({ 
+    {q0,t1,q1}, {q1,t0,q1},
+    {q1,t1,q2}, {q2,t0,q0},
+    {q2,t0,q0}
+  }, q0).
+
+% {From, {{state, token}, nextstate}}
 smachine(PossibleStates, CurrentState) ->
   receive
     terminate -> ok
